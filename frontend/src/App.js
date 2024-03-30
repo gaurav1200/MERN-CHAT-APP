88230ae68393ad334354db05c06a1719159639f0
Gaurav S Patil <gauravspatil965@gmail.com>
2024-03-30 17:36:41 +0530

    added website visit counter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,24 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
+import axios from "axios";
 
+const visitEndpoint = "https://gsp-server.onrender.com";
 function App() {
+  useEffect(() => {
+    const data = {
+      webSiteName: "chat-app",
+      count: 1,
+    };
+    axios
+      .post(`${visitEndpoint}/visit`, { data })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="App">
       <Routes>
